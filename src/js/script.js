4c6245322a3f47d722279ec987ae5f4139210d66
peter <forLichess123@yandex.ru>
2020-10-29 10:39:05 +0300

    add comment block

--- src/js/script.js
+++ src/js/script.js
@@ -8,25 +8,7 @@
     slideBy: 'page',
     autoplay: false,
     controls: false,
-    nav: false,
-    responsive: {
-        1200:{
-            nav: false
-        },
-        992: {
-            nav: false
-        },
-        767: {
-            nav: true
-        },
-        575: {
-            nav: true
-        },
-        320: {
-            nav: true
-        }
-      
-      }
+    nav: true
   });
   document.querySelector('.prev').addEventListener('click',() => {
     slider.goTo('prev');
